927bb6628313186d77f1e13742776ff17aba8ea7
doananhkien3007-pixel <doananhkien3007@gmail.com>
2026-09-05 22:19:20 +0700

    d

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config, { isServer }) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-      crypto: false,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
